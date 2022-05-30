cd6f6479de7706c7fba16644bc99b8fc3feb6dfd
Jereamy Smothers <jereamyasmothers@gmail.com>
2022-05-30 15:49:25 -0700

    Added html and psuedo-code for JS

---
+++ assets/JS/script.js
@@ -0,0 +1,25 @@
+// code quiz
+
+// implement a function where questions will be cycled through with 
+// functions in of themselves; a button will initiate a countdown (function) 
+// with a scoring function for high scores; include a game over and would you
+// like to play again function.
+
+// gloabal variables
+
+// Overall function linked to html buttons for starting one of 4 quizzes
+
+    // local variables
+        // Function for countdown
+        // Function for timer during quiz; synced to scoring
+            // Function for storing scores with name input available
+        // Function for questions to cycle
+            // Function for question 1 (entails boolean with one correct answer and score addition)
+            // Function for question 2
+            // Function for question 3
+            // Function for question 4
+        
+
+// addeventlistener(click)
+
+// function accessing high scores
